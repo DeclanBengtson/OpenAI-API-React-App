de065cab967fe0b873de4d7cb0342eb9e4a2d710
Declan Bengtson <n11079550@qut.edu.au>
2023-06-29 18:39:50 +1000

    Add pages

--- src/Pages/MealPlan.jsx
+++ src/Pages/MealPlan.jsx
@@ -1,6 +1,5 @@
-import React from "react"
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
-
 import { Configuration, OpenAIApi } from "openai";
 
 
@@ -10,14 +9,18 @@ const OPENAI_API_KEY ="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";
 const configuration = new Configuration({
     apiKey: OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
 
-export default function Workout() {
 
+
+export default function MealPlan() {
+    
     return (
         <div>
-            <p>Workout Page</p>
-        </div>
+            <p>Meal Plan Page</p>
+            <textarea classname="text-area" cols={100} rows={10}></textarea>
+            <p></p>
+            <button classname="button" onClick={}>Generate Meal Plan</button>
+            </div>
     );
 }
